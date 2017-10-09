ce8506f2ffb4a69869bb86f4a6f68e799880433d
Raffaele Mancino <raffaele.linux@gmail.com>
2017-10-09 17:37:05 +0200

    start test git

--- src/jframework/Jframework.java
+++ src/jframework/Jframework.java
@@ -15,7 +15,7 @@ public class Jframework {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("jframework.Jframework.main()");
     }
     
 }
